4755380f4b8fb75b52c0ce3f4e148097f2f5da9d
iran <iranilsonhonorato88@gmail.com>
2025-11-25 18:59:05 -0300

    fix: otimizando códgio do langchain

--- extracao.py
+++ extracao.py
@@ -1,74 +1,129 @@
 import os
 import json
+import requests 
 from operator import itemgetter
 from enum import Enum
 from dotenv import load_dotenv
-from openai import OpenAI
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts import MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from pydantic import Field, BaseModel, ValidationError
 
+# Baseie-se nos exemplos abaixo para fazer a análise do trabalho acima:
+#     Exemplo 1: 
+#     O conteúdo hospedado na seguinte URL: https://periodicos.fgv.br/rbe/article/view/84448
+    
+#     'classificacao': 'Acadêmica',
+#     'metodologia': 'Quantitativa',
+#     'area_avaliada': 'Meio Ambiente'
+#     'ods': '3; 11',
+#     'etapa': 'Certificação', 
+#     'titulo': 'When streets have no men: Urban traffic restrictions and air pollution'
+
+#     Exemplo 2: 
+#     O conteúdo hospedado na seguinte URL: https://www.sciencedirect.com/science/article/abs/pii/S0165176523005281?via%3Dihub
+    
+#     'classificacao': 'Acadêmica',
+#     'metodologia': 'Quantitativa',
+#     'area_avaliada': 'Desenvolvimento Social'
+#     'ods': '2; 8; 11',
+#     'etapa': 'Mapeamento de Determinantes', 
+#     'titulo': 'Technical change in agriculture and homicides: The case of genetically-modified soy seeds in Brazil'
+
+#     Exemplo 3: 
+#     O conteúdo hospedado na seguinte URL: https://repositorio.insper.edu.br/entities/publication/1e8eab33-99e1-4a71-89c4-290b5044a4f7
+    
+#     'classificacao': 'Acadêmica',
+#     'metodologia': 'Mista',
+#     'area_avaliada': 'Educação'
+#     'ods': '3; 4; 10; 11',
+#     'etapa': 'Mapeamento dos Determinantes; Definição e Dimensão; Certificação.', 
+#     'titulo': 'Essays on Education and Health Economics'
+
+#     Exemplo 4:
+#     O conteúdo hospedado na seguinte URL: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0289604
+
+#     'classificacao': 'Acadêmica',
+#     'metodologia': 'Quantitativa',
+#     'area_avaliada': 'Saúde'
+#     'ods': '3',
+#     'etapa': 'Mapeamento dos Determinantes', 
+#     'titulo': 'The relationship between staying at home during the pandemic and the number of conceptions: A national panel data analysis'
 
 
+#     Exemplo 5: 
+#     O conteúdo hospedado na seguinte URL: https://ieps.org.br/panorama-ieps-01/
+#     'classificacao': 'Técnica', 
+#     'metodologia': 'Quantitativa', 
+#     'area_avaliada': 'Saúde', 
+#     'ods': '3; 11', 
+#     'etapa': 'Definição e Dimensão', 
+#     'titulo': 'Panorama da Cobertura Vacinal no Brasil, 2020'
 
+#     Exemplo 6:
+#     O conteúdo hospedado na seguinte URL: https://onlinelibrary.wiley.com/doi/10.1002/hec.4241
+#     'classificacao': 'Acadêmica', 
+#     'metodologia': 'Quantitativa', 
+#     'area_avaliada': 'Saúde', 
+#     'ods': '3; 8; 10', 
+#     'etapa': 'Definição e Dimensão', 
+#     'titulo': 'Financing needs, spending projection, and the future of health in Brazil'
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=OPENAI_API_KEY) 
+
+
+class DesafiosODS(Enum):
+    ODS_01 = "Erradicação da pobreza"
+    ODS_02 = "Fome zero e agricultura sustentável"
+    ODS_03 = "Saúde e bem-estar"
+    ODS_04 = "Educação de qualidade"
+    ODS_05 = "Igualdade de gênero"
+    ODS_06 = "Água potável e saneamento"
+    ODS_07 = "Energia limpa e acessível"
+    ODS_08 = "Trabalho decente e crescimento econômico"
+    ODS_09 = "Indústria, inovação e infraestrutura"
+    ODS_10 = "Redução das desigualdades"
+    ODS_11 = "Cidades e comunidades sustentáveis"
+    ODS_12 = "Consumo e produção sustentáveis"
+    ODS_13 = "Ação contra a mudança global do clima"
+    ODS_14 = "Vida na água"
+    ODS_15 = "Vida terrestre"
+    ODS_16 = "Paz, justiça e instituições eficazes"
+    ODS_17 = "Parcerias e meios de implementação"
 
 
 
-# class DesafiosODS(Enum):
-#     ODS_01 = "Erradicação da pobreza"
-#     ODS_02 = "Fome zero e agricultura sustentável"
-#     ODS_03 = "Saúde e bem-estar"
-#     ODS_04 = "Educação de qualidade"
-#     ODS_05 = "Igualdade de gênero"
-#     ODS_06 = "Água potável e saneamento"
-#     ODS_07 = "Energia limpa e acessível"
-#     ODS_08 = "Trabalho decente e crescimento econômico"
-#     ODS_09 = "Indústria, inovação e infraestrutura"
-#     ODS_10 = "Redução das desigualdades"
-#     ODS_11 = "Cidades e comunidades sustentáveis"
-#     ODS_12 = "Consumo e produção sustentáveis"
-#     ODS_13 = "Ação contra a mudança global do clima"
-#     ODS_14 = "Vida na água"
-#     ODS_15 = "Vida terrestre"
-#     ODS_16 = "Paz, justiça e instituições eficazes"
-#     ODS_17 = "Parcerias e meios de implementação"
-
-
-# class ODSNumber(Enum):
-#     ODS_01 = 1
-#     ODS_02 = 2
-#     ODS_03 = 3
-#     ODS_04 = 4
-#     ODS_05 = 5
-#     ODS_06 = 6
-#     ODS_07 = 7
-#     ODS_08 = 8
-#     ODS_09 = 9
-#     ODS_10 = 10
-#     ODS_11 = 11
-#     ODS_12 = 12
-#     ODS_13 = 13
-#     ODS_14 = 14
-#     ODS_15 = 15
-#     ODS_16 = 16
-#     ODS_17 = 17
+class ODSNumber(Enum):
+    ODS_01 = 1
+    ODS_02 = 2
+    ODS_03 = 3
+    ODS_04 = 4
+    ODS_05 = 5
+    ODS_06 = 6
+    ODS_07 = 7
+    ODS_08 = 8
+    ODS_09 = 9
+    ODS_10 = 10
+    ODS_11 = 11
+    ODS_12 = 12
+    ODS_13 = 13
+    ODS_14 = 14
+    ODS_15 = 15
+    ODS_16 = 16
+    ODS_17 = 17
 
 
 
-# class EtapaCicloPP(Enum):
-#     DEFINICAO = "Definição e Dimensão"
-#     MOBILIZACAO = "Mobilização"
-#     MAPEAMENTO = "Mapeamento dos Determinantes"
-#     SOLUCAO = "Solução"
-#     JUSTIFICATIVA = "Justificativa"
-#     APRIMORAMENTO = "Aprimoramento"
-#     CERTIFICACAO = "Certificação"
+class EtapaCicloPP(Enum):
+    DEFINICAO = "Definição e Dimensão"
+    MOBILIZACAO = "Mobilização"
+    MAPEAMENTO = "Mapeamento dos Determinantes"
+    SOLUCAO = "Solução"
+    JUSTIFICATIVA = "Justificativa"
+    APRIMORAMENTO = "Aprimoramento"
+    CERTIFICACAO = "Certificação"
 
 
 class Classificacao(Enum):
@@ -101,16 +156,13 @@ class Trabalho(BaseModel):
     titulo:str = Field(..., description="Titulo do trabalho")
 
 
-def extracao_langchain(texto: str) -> Trabalho:
-    system_text = """
-    Você é um pesquisador especializado em políticas públicas e nos Objetivos de Desenvolvimento Sustentável (ODS) da ONU. 
-    Seu papel é analisar textos, artigos e relatórios a partir de uma perspectiva acadêmica e técnica, utilizando uma abordagem baseada em evidências. 
-    Sempre que receber um texto ou estudo, realize a seguinte tarefa: Responda **apenas** com um objeto JSON válido que tenha as seguintes chaves: 
-    - classificacao: "Academica" (maior rigor acadêmico nos métodos e na escrita) ou "Tecnica" (ênfase à execução, procedimentos e resultados práticos, mais do que à fundamentação teórica) 
-    - metodologia: "Qualitativa", "Quantitativa" ou "Mista" 
-    - area_avaliada: "Educação", "Saúde", "Meio Ambiente", "Gênero", "Raça", "Pobreza" ou "Desenvolvimento Social" 
-    - ods: o número das ODS's (Objetivo de Desenvolvimento Sustentável da ONU) relacionadas ao trabalho no formato textual (Podem ter mais de uma relacionadas)
-    - etapa do ciclo de políticas públicas (Podem ter mais de uma relacionadas): 
+
+
+
+
+def extracao_langchain(texto:str) -> Trabalho:
+
+    etapa_ciclo_pp = '''
         1. "Definição e Dimensão": É o ponto de partida de toda política pública. Consiste em especificar claramente o problema social que se deseja resolver, medir sua magnitude e suas consequências. Inclui três subetapas:
         Especificação e mensuração: definir o problema e os resultados de interesse.
         Magnitude e evolução: avaliar o tamanho do problema e sua evolução no tempo e entre grupos.
@@ -152,108 +204,81 @@ def extracao_langchain(texto: str) -> Trabalho:
         Relações custo-benefício e custo-efetividade;
         Adequação (a solução atendeu bem à população-alvo?).
         A certificação assegura que a política alcançou seus objetivos dentro do orçamento e gera evidências para outras iniciativas 
+    '''
+
+
+    ods_description = '''
+        1 - Erradicação da pobreza: acabar com a pobreza em todas as suas formas, em todos os lugares;
+        2 - Fome zero e agricultura sustentável: acabar com a fome, alcançar a segurança alimentar, melhoria da nutrição e promover a agricultura sustentável;
+        3 - Saúde e bem-estar: assegurar uma vida saudável e promover o bem-estar para todas e todos, em todas as idades;
+        4 - Educação de qualidade: assegurar a educação inclusiva e equitativa e de qualidade, e promover oportunidades de aprendizagem ao longo da vida para todas e todos;
+        5 - Igualdade de gênero: alcançar a igualdade de gênero e empoderar todas as mulheres e meninas;
+        6 - Água potável e saneamento: assegurar a disponibilidade e gestão sustentável da água e saneamento para todas e todos;
+        7 - Energia Limpa e acessível: assegurar o acesso confiável, sustentável, moderno e a preço acessível à energia para todas e todos;
+        8 - Trabalho decente e crescimento econômico: promover o crescimento econômico sustentado, inclusivo e sustentável, emprego pleno e produtivo e trabalho decente para todas e todos;
+        9 - Indústria, Inovação e Infraestrutura: Construir infraestruturas resilientes, promover a industrialização inclusiva e sustentável e fomentar a inovação;
+        10 - Redução das desigualdades: reduzir a desigualdade dentro dos países e entre eles;
+        11 - Cidades e comunidades sustentáveis: tornar as cidades e os assentamentos humanos inclusivos, seguros, resilientes e sustentáveis;
+        12 - Consumo e produção responsáveis: assegurar padrões de produção e de consumo sustentáveis;
+        13 - Ação contra a mudança global do clima: tomar medidas urgentes para combater a mudança climática e seus impactos;
+        14 - Vida na água: conservação e uso sustentável dos oceanos, dos mares e dos recursos marinhos para o desenvolvimento sustentável;
+        15 - Vida terrestre: proteger, recuperar e promover o uso sustentável dos ecossistemas terrestres, gerir de forma sustentável as florestas, combater a desertificação, deter e reverter a degradação da terra e deter a perda de biodiversidade;
+        16 - Paz, justiça e instituições eficazes: promover sociedades pacíficas e inclusivas para o desenvolvimento sustentável, proporcionar o acesso à justiça para todos e construir instituições eficazes, responsáveis e inclusivas em todos os níveis;
+        17 - Parcerias e meios de implementação: fortalecer os meios de implementação e revitalizar a parceria global para o desenvolvimento sustentável
+    '''
+
+
+    system_text = f"""
+    Você é um pesquisador especializado em políticas públicas e nos Objetivos de Desenvolvimento Sustentável (ODS) da ONU. 
+    Seu papel é analisar textos, artigos e relatórios a partir de uma perspectiva acadêmica e técnica, utilizando uma abordagem baseada em evidências. 
+    Sempre que receber um texto ou estudo, realize a seguinte tarefa: Responda **apenas** com um objeto JSON válido que tenha as seguintes chaves: 
+    - classificacao: "Academica" (maior rigor acadêmico nos métodos e na escrita) ou "Tecnica" (ênfase à execução, procedimentos e resultados práticos, mais do que à fundamentação teórica) 
+    - metodologia: "Qualitativa", "Quantitativa" ou "Mista" 
+    - area_avaliada: "Educação", "Saúde", "Meio Ambiente", "Gênero", "Raça", "Pobreza" ou "Desenvolvimento Social" 
+    - ods: o número das ODS's (Objetivo de Desenvolvimento Sustentável da ONU) relacionadas ao trabalho no formato textual (Podem ter mais de uma relacionadas):{ods_description}
+    - etapa do ciclo de políticas públicas (Podem ter mais de uma relacionadas): {etapa_ciclo_pp}
     - titulo: Extraia o título do trabalho (Apenas copie e cole o título. Não precisa traduzir)
 
     Responda somente com um JSON **válido**, utilizando **aspas duplas** em todas as chaves e valores, e nada mais. O formato deve ser exatamente este:
 
-    {{
+    <<
     "classificacao": "...",
     "metodologia": "...",
     "area_avaliada": "...",
     "ods": "...",
     "etapa": "...",
     "titulo": "..."
-    }}
-
-    
+    >>
     """ 
 
+
     user_template = '''
-    Trabalho:
-    {texto}
-
-    Baseie-se nos exemplos abaixo para fazer a análise do trabalho acima:
-    Exemplo 1: 
-    O conteúdo hospedado na seguinte URL: https://periodicos.fgv.br/rbe/article/view/84448
-    
-    'classificacao': 'Acadêmica',
-    'metodologia': 'Quantitativa',
-    'area_avaliada': 'Meio Ambiente'
-    'ods': '3; 11',
-    'etapa': 'Certificação', 
-    'titulo': 'When streets have no men: Urban traffic restrictions and air pollution'
-
-    Exemplo 2: 
-    O conteúdo hospedado na seguinte URL: https://www.sciencedirect.com/science/article/abs/pii/S0165176523005281?via%3Dihub
-    
-    'classificacao': 'Acadêmica',
-    'metodologia': 'Quantitativa',
-    'area_avaliada': 'Desenvolvimento Social'
-    'ods': '2; 8; 11',
-    'etapa': 'Mapeamento de Determinantes', 
-    'titulo': 'Technical change in agriculture and homicides: The case of genetically-modified soy seeds in Brazil'
-
-    Exemplo 3: 
-    O conteúdo hospedado na seguinte URL: https://repositorio.insper.edu.br/entities/publication/1e8eab33-99e1-4a71-89c4-290b5044a4f7
-    
-    'classificacao': 'Acadêmica',
-    'metodologia': 'Mista',
-    'area_avaliada': 'Educação'
-    'ods': '3; 4; 10; 11',
-    'etapa': 'Mapeamento dos Determinantes; Definição e Dimensão; Certificação.', 
-    'titulo': 'Essays on Education and Health Economics'
-
-    Exemplo 4:
-    O conteúdo hospedado na seguinte URL: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0289604
-
-    'classificacao': 'Acadêmica',
-    'metodologia': 'Quantitativa',
-    'area_avaliada': 'Saúde'
-    'ods': '3',
-    'etapa': 'Mapeamento dos Determinantes', 
-    'titulo': 'The relationship between staying at home during the pandemic and the number of conceptions: A national panel data analysis'
-
-
-    Exemplo 5: 
-    O conteúdo hospedado na seguinte URL: https://ieps.org.br/panorama-ieps-01/
-    'classificacao': 'Técnica', 
-    'metodologia': 'Quantitativa', 
-    'area_avaliada': 'Saúde', 
-    'ods': '3; 11', 
-    'etapa': 'Definição e Dimensão', 
-    'titulo': 'Panorama da Cobertura Vacinal no Brasil, 2020'
-
-    Exemplo 6:
-    O conteúdo hospedado na seguinte URL: https://onlinelibrary.wiley.com/doi/10.1002/hec.4241
-    'classificacao': 'Acadêmica', 
-    'metodologia': 'Quantitativa', 
-    'area_avaliada': 'Saúde', 
-    'ods': '3; 8; 10', 
-    'etapa': 'Definição e Dimensão', 
-    'titulo': 'Financing needs, spending projection, and the future of health in Brazil'
+        Trabalho:
+        {texto}
     '''
 
 
-    # Estruturação do prompt
+    # Orientando a resposta do modelo com um contexto como prompt
     prompt = ChatPromptTemplate.from_messages([
         ("system", system_text),
         ("user", user_template)
     ])
 
-    # Intância do modelo de linguagem utilizada
-    llm = ChatOpenAI(model="gpt-4o", temperature=0.3) 
 
+    # Intância do modelo de linguagem utilizada
+    llm = ChatOpenAI(api_key=OPENAI_API_KEY, model="gpt-4.1", temperature=0.3) 
+    structured_output = llm.with_structured_output(Trabalho)
     # Orienta o langchain a devolver uma reposta diretamente convertida em um objeto do tipo Trabalho
-    structured_llm = llm.with_structured_output(Trabalho)
+
+
 
     # Encadeamento do texto com o prompt e a resposta estruturada que desejamos 
-    chain = ({"texto": itemgetter("texto")} | prompt | structured_llm)
+    chain = ({"input": itemgetter("input")} | prompt | structured_output)
+
 
     try:
-        # Invoca tudo: pega o seu texto, monta o prompt, chama o GPT e devolve um objeto (mas o encadeamento texto, prompt e structured_llm é feito pelo chain)
-        # [System]: Você é um pesquisador especializado...
-        # [User]: TEXTO: texto(passado no argumento)
-        resposta = chain.invoke({"texto": texto})
+        # Paga o nosso no input e encadeia (com o chain) com o contexto
+        resposta = chain.invoke({"input": texto})
         resultado = {
             "classificacao": resposta.classificacao.value,
             "metodologia": resposta.metodologia.value,
@@ -263,7 +288,6 @@ def extracao_langchain(texto: str) -> Trabalho:
             "titulo": resposta.titulo
         }
         
-        # Se tudo der certo, `resposta` deve ser uma instância de Trabalho
         return resultado
 
         
@@ -273,12 +297,10 @@ def extracao_langchain(texto: str) -> Trabalho:
 
 
 
-# if __name__ == "__main__":
-#     exemplo = """
-# https://onlinelibrary.wiley.com/doi/10.1002/sd.2754
-#     """
-
-#     resultado = extracao_langchain(exemplo)
-#     print(resultado)
+if __name__ == "__main__":
+    exemplo = """Resumo
+In this paper, we analyze two quasi-experiments to assess how urban traffic restrictions and social distancing norms affect pollution levels in the municipality São Paulo, one of the largest metropolitan areas in the world. First, using hourly air pollution levels measured in thirty-three monitoring stations in the state of São Paulo, we exploit exogenous variation in quarantine rules following the COVID-19 outbreak to estimate how social distancing norms affected pollution levels across different municipalities. We find an average decrease of 22.4% in air pollution after the first days of the capital's quarantine announcement, with heterogeneous effects across pollutants, driven by decreases in the vehicle fleet and urban mobility. Second, we compare this effect with another quasi-experiment that explores exogenous suspensions of traffic restriction rules between 2000-2018 in the municipality of São Paulo. We also document increases in pollution levels when more cars are allowed in the streets, with an average increase of 16.7% in air pollution. Finally, we use our estimates to show that a reduction of 4.7 times the estimated ATT in the quarantine period is necessary to reach the capital's long-term air quality goals."""
+    resultado = extracao_langchain(exemplo)
+    print(resultado)
 
     
